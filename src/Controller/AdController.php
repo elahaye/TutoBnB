0d1d645b07810ca7dfc064236cdb49063cda13be
elahaye <e.lahaye49@gmail.com>
2020-11-09 15:43:04 +0100

    First Commit

---
+++ src/Controller/AdController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ad;
+use App\Form\AdType;
+use App\Repository\AdRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdController extends AbstractController
+{
+    /**
+     * @Route("/ads", name="ads_index")
+     */
+    public function index(AdRepository $adRepository): Response
+    {
+        $ads = $adRepository->findAll();
+
+        return $this->render('ad/index.html.twig', [
+            'ads' => $ads,
+        ]);
+    }
+
+    /**
+     * Permet de créer une annonce
+     * 
+     * @Route("ads/new", name="ads_create")
+     */
+    public function create(Request $request, EntityManagerInterface $manager): Response
+    {
+        $ad = new Ad();
+
+        $form = $this->createForm(AdType::class, $ad);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            foreach ($ad->getImages() as $image) {
+                $image->setAd($ad);
+                $manager->persist($image);
+            }
+
+            $manager->persist($ad);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "L'annonce <strong>{$ad->getTitle()}</strong> a bien été enregistrée !"
+            );
+
+            return $this->redirectToRoute('ads_show', [
+                'slug' => $ad->getSlug()
+            ]);
+        }
+
+        return $this->render('ad/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * Permet d'afficher le formulaire d'édition
+     * 
+     * @Route("/ads/{slug}/edit", name="ads_edit")
+     * 
+     * @return Response
+     */
+    public function edit(Ad $ad, Request $request, EntityManagerInterface $manager): Response
+    {
+        $form = $this->createForm(AdType::class, $ad);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            foreach ($ad->getImages() as $image) {
+                $image->setAd($ad);
+                $manager->persist($image);
+            }
+
+            $manager->persist($ad);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "Les modifications de l'annonce <strong>{$ad->getTitle()}</strong> ont bien été enregistrées !"
+            );
+
+            return $this->redirectToRoute('ads_show', [
+                'slug' => $ad->getSlug()
+            ]);
+        }
+
+        return $this->render('ad/edit.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * Permet d'afficher une seule annonce
+     * 
+     * @Route("/ads/{slug}", name="ads_show")
+     */
+    public function show(Ad $ad): Response
+    {
+        // Je récupère l'annonce qui corresponse au slug
+        // $ad = $adRepository->findOneBySlug($slug);
+
+        return $this->render('ad/show.html.twig', [
+            'ad' => $ad
+        ]);
+    }
+}
